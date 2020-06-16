f04bc9667f6a00b94bb5c1647dbed7f4334f9eac
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-06-16 11:31:41 +0200

    claim

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'description': """Medical """,
     'author': "Black Belts Egypt",
     'website': "www.blackbelts-egypt.com",
-    'category': 'plat',
+    'category': 'Medical',
     'version': '0.1',
     'license': 'AGPL-3',
     # any module necessary for this one to work correctly
@@ -14,7 +14,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-
+        'security/security.xml',
         'views/setup.xml',
         'views/medical_helpdesk.xml',
         'views/menu_item.xml',
