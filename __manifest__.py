f6aeb2f4b910adfd28db1853de695bc9150cc362
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-11-04 15:41:24 +0200

    update

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
     'version': '0.1',
     'license': 'AGPL-3',
     # any module necessary for this one to work correctly
-    'depends': ['base','helpdesk_inherit',],
+    'depends': ['base','helpdesk_inherit','arope_conf'],
 
     # always loaded
     'data': [
@@ -19,7 +19,7 @@
         'views/setup.xml',
         'views/covers.xml',
 
-        # 'views/menu_item.xml',
+        'views/menu_item.xml',
 
 
     ],
